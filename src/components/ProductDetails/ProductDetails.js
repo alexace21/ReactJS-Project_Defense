ccd7d560c24482f944b0f8a40cfdbcd7767259bc
Alex Asenov <alex.ace7ov@gmail.com>
2022-08-09 14:48:46 +0000

    Align Css in Productdetails page

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -11,14 +11,22 @@ const ProductDetails = () => {
             </div>
 
             <div className={styles.rightColumn}>
-                <h1>Product Title</h1>
-                <span className={styles.price}> </span>
-                <span className={styles['actual-price']}>130.00$</span>
-                <p className={styles['box-description']}>Description</p>
-                <div className={styles['text-box']}>
-                    <div disabled contentEditable='false' >
-                        <p className={styles.productRate}>Product rating...</p>
+                <div className={styles['box-summary']}>
+                    <h1>Product Title: </h1>
+                    <span className={styles.price}> </span>
+                    <span className={styles['actual-price']}>Price: 130.00$</span>
+                    <p className={styles['box-description']}>Description: </p>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <div className={styles['text-box']}>
+                        <div disabled contentEditable='false' >
+                            <p className={styles.productRate}>This product has been liked: 5 times.</p>
+                        </div>
                     </div>
+
                 </div>
 
                 <div className={styles['box-rating']}>
@@ -30,60 +38,46 @@ const ProductDetails = () => {
                         <span className={styles['rating-star']} data-index="4"></span>
                         <span className={styles['rating-star']} data-index="5"></span>
                     </div>
-                    <output aria-hidden="true" className={styles['rating-count']}>22</output>
                 </div>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                
-
             </div>
-            {/* <div className={styles['default-page']}>
-                <div className={styles.row}>
-                    
-                        <div className={styles['buy-box']}>
-                            <div className={styles['buy-boxImage']}>
-                                <div className={styles.image}>
-                                    <picture>
-                                        <img src='./images/camera.jpg' />
-                                    </picture>
-                                </div>
-                            </div>
-                            <div className={styles['buy-content']}>
-
-                            </div>
-                        </div>
-
-                        <div className={styles['product-content']}>
-                            <h1>Product Title</h1>
-                            <span className={styles.price}>
-                                <span className={styles['actual-price']}>130.00$</span>
-                                <p className={styles['box-description']}>Description</p>
-                                <div className={styles['text-box']}>
-                                    <div disabled contentEditable='false' >
-                                        <p>Product rating...</p>
-                                    </div>
-                                </div>
-
-                                <div className={styles['box-rating']}>
-                                    <div className={styles['rating-range']}>Ratings and reviews section. Average 3.8 of 5 stars</div>
-                                    <div className={styles['rating-group']}>
-                                        <span className={styles['rating-star']} data-index="1"></span>
-                                        <span className={styles['rating-star']} data-index="2"></span>
-                                        <span className={styles['rating-star']} data-index="3"></span>
-                                        <span className={styles['rating-star']} data-index="4"></span>
-                                        <span className={styles['rating-star']} data-index="5"></span>
-                                    </div>
-                                    <output aria-hidden="true" className={styles['rating-count']}>22</output>
-                                </div>
-                            </span>
-                        </div>
-                    
-                </div>
-            </div> */}
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
         </div>
     )
 };
