bfb02ec2090bf34688eb17f1c738b133eac9f80b
Alex Asenov <alex.ace7ov@gmail.com>
2022-08-09 15:00:00 +0000

    Latest fixes

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -26,18 +26,7 @@ const ProductDetails = () => {
                             <p className={styles.productRate}>This product has been liked: 5 times.</p>
                         </div>
                     </div>
-
-                </div>
-
-                <div className={styles['box-rating']}>
-                    <div className={styles['rating-range']}>Ratings and reviews section. Average 3.8 of 5 stars</div>
-                    <div className={styles['rating-group']}>
-                        <span className={styles['rating-star']} data-index="1"></span>
-                        <span className={styles['rating-star']} data-index="2"></span>
-                        <span className={styles['rating-star']} data-index="3"></span>
-                        <span className={styles['rating-star']} data-index="4"></span>
-                        <span className={styles['rating-star']} data-index="5"></span>
-                    </div>
+                    <button type='submit' className={styles['submit-btn']}>Like this product!</button>
                 </div>
             </div>
             <br />
